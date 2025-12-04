ecafb445ec9734c6be4f887c1c93b74b583fd5b5
ROHIT-code-6363 <rohitsd6363@gmail.com>
2025-12-04 09:00:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,14 @@ const cors = require("cors");
 const app = express();
 const router = require("./router/auth-router");
 const connectDB = require("./utils/db");
+const https = require('https');
 
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
-const Port = process.env.PORT || 5000;
-
-// Isse import karna zaroori hai
-const https = require('https');
-
 // Apna Render wala URL yahan likhein
-const serverUrl = "https://Price.log.onrender.com/api/auth/products"; 
+const serverUrl = "https://price.log.onrender.com/api/auth/products"; 
 
 // setInterval use karein taaki ye har 14 minute mein repeat ho
 setInterval(() => {
@@ -24,6 +20,7 @@ setInterval(() => {
     });
 }, 1 * 60 * 1000); // 14 Minutes
 
+const Port = process.env.PORT || 5000;
 
 connectDB().then(() => {
 app.listen(Port, () => {
